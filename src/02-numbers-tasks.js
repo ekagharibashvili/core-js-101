d4e1bc789e9e04930c84497d3128899fda7b3b65
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-09-24 22:01:23 +0400

    push

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -130,8 +130,16 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  let angle;
+  if (x1 !== x2 && y1 !== y2) {
+    angle = Math.PI / 2;
+  } else if (x1 === x2 && y1 === -y2) {
+    angle = Math.PI;
+  } else {
+    angle = 0;
+  }
+  return angle;
 }
 
 /**
@@ -146,8 +154,10 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const arr = value.toString().split('');
+  const index = arr.length - 1;
+  return arr[index];
 }
 
 
@@ -162,8 +172,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -179,8 +189,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
