843b7223a3bc81f0cdcd417c2129fee399174b01
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-09-27 19:42:40 +0400

    feat: push

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -210,8 +210,8 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * (10 ** pow);
 }
 
 /**
@@ -231,9 +231,16 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /**
  * Tries to convert value to number and returns it if conversion was successfull;
